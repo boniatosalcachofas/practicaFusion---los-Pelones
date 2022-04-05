acdc4852266d34367f587395e2147d436c28c763
'Jose' <josemgodoyvictor@gmail.com>
2022-04-05 18:21:18 +0200

    Version 2
    ColorPelo

--- src/practicaFusion/Mascota.java
+++ src/practicaFusion/Mascota.java
@@ -2,6 +2,7 @@ package practicaFusion;
 
 public class Mascota {
 	private String nombre;
+	private String colorPelaje;
 
 	public Mascota(String nombre) {
 		this.nombre = nombre;
--- src/practicaFusion/Principal.java
+++ src/practicaFusion/Principal.java
@@ -3,8 +3,11 @@ package practicaFusion;
 public class Principal {
 	public static void main(String[] args) {
 		
-		
+	Gato Gatico = new Gato ("Mishifu");
 	
+	Gatico.MandarMensaje();
+	
+		
 
 	}
 
