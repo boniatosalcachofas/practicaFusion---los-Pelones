90281c2a9731a6526799c39fb985795a9a60af70
'Jose' <josemgodoyvictor@gmail.com>
2022-04-06 20:05:07 +0200

    Version 3
    Mod Clase Principal

--- src/practicaFusion/Principal.java
+++ src/practicaFusion/Principal.java
@@ -3,7 +3,7 @@ package practicaFusion;
 public class Principal {
 	public static void main(String[] args) {
 
-		Gato Gatico = new Gato("Mishifu", "Blanco");
+		Gato Gatico = new Gato("Mishifu", "Negro");
 
 		Gatico.MandarMensaje();
 
