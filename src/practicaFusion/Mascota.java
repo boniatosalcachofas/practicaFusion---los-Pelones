c8e7092d9f2c0961d51e55f4fec1a270c5f11c46
'Jose' <josemgodoyvictor@gmail.com>
2022-04-06 20:46:23 +0200

    Parte II punto 4 JoseM
    Pongo color en MandarMensaje

--- src/practicaFusion/Mascota.java
+++ src/practicaFusion/Mascota.java
@@ -17,7 +17,7 @@ public class Mascota {
 	}
 
 	public void MandarMensaje() {
-		System.out.println("Soy " + this.nombre + " y te voy a contar algo o no");
+		System.out.println("Soy Jose Manuel y te voy a contar algo o no y soy de color Negro");
 	}
 
 }
